3d5b01f432175c0846414ba1f3f65e096446873e
ksaladi <69457674+ksaladi@users.noreply.github.com>
2022-06-30 18:54:52 +0530

    Revert "[charts/portal] : merging 5.1.1 changes to stable"

--- .github/helm-repo.sh
+++ .github/helm-repo.sh
@@ -4,6 +4,5 @@ set -euxo pipefail
 helm repo add hazelcast "https://hazelcast-charts.s3.amazonaws.com/"
 helm repo add influx "https://helm.influxdata.com/"
 helm repo add bitnami "https://charts.bitnami.com/bitnami"
-helm repo add bitnami-full "https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami"
 helm repo add stable "https://charts.helm.sh/stable"
 helm repo add ingress-nginx "https://kubernetes.github.io/ingress-nginx/"
